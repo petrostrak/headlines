d2a77f2e7b31df536e1746ab6d298cbf24438a3b
petrostrak <pit.trak@gmail.com>
2022-12-14 23:08:13 +0200

    First req to news api and article struct

--- news_api/src/main.rs
+++ news_api/src/main.rs
@@ -1,3 +1,28 @@
+use std::error::Error;
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Articles {
+    articles: Vec<Article>,
+}
+
+#[derive(Deserialize)]
+struct Article {
+    title: String,
+    url: String,
+}
+
+fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
+    let response = ureq::get(url).call()?.into_string()?;
+
+    dbg!(response);
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let url =
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=7df214ecd666464198e34e3ca13f77bd";
+
+    let articles = get_articles(url);
 }
