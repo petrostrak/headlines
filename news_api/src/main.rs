b8a0040f88422a7a8354e4381183fd035ef1d4ab
petrostrak <pit.trak@gmail.com>
2022-12-14 23:22:17 +0200

    Add color to the terminal

--- news_api/src/main.rs
+++ news_api/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use colour::{dark_green, yellow};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -17,13 +18,23 @@ fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
     let response = ureq::get(url).call()?.into_string()?;
 
     let articles: Articles = serde_json::from_str(&response)?;
-    dbg!(articles);
-    todo!()
+    Ok(articles)
 }
 
-fn main() {
+fn render_articles(articles: &Articles) {
+    for i in &articles.articles {
+        dark_green!("> {}\n", i.title);
+        yellow!("- {}\n\n", i.url);
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let url =
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=7df214ecd666464198e34e3ca13f77bd";
 
-    let articles = get_articles(url);
+    let articles = get_articles(url)?;
+
+    render_articles(&articles);
+
+    Ok(())
 }
