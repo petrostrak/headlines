d09718b804c51dbbb7ac46899bf007a146c09ddd
petrostrak <pit.trak@gmail.com>
2022-12-15 15:35:07 +0200

    swap colour crate with termimad

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use colour::{dark_green, yellow};
+mod theme;
+
 use dotenv::dotenv;
 use newsapi::{get_articles, Articles};
 use std::error::Error;
 
 fn render_articles(articles: &Articles) {
+    let theme = theme::default();
+    theme.print_text("# Top headlines\n\n");
     for i in &articles.articles {
-        dark_green!("> {}\n", i.title);
-        yellow!("- {}\n\n", i.url);
+        theme.print_text(&format!("`{}`", i.title));
+        theme.print_text(&format!("> *{}*", i.url));
+        theme.print_text("---");
     }
 }
 
