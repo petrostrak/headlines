539c0c99ce338adba397424623ec5615558e3c1e
petrostrak <pit.trak@gmail.com>
2022-12-16 18:54:16 +0200

    Add prepare_url helper

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,4 +1,7 @@
 use serde::Deserialize;
+use url::Url;
+
+const Base_URL: &str = "https://newsapi.org/v2";
 
 #[derive(thiserror::Error, Debug)]
 pub enum NewsApiError {
@@ -8,6 +11,8 @@ pub enum NewsApiError {
     FailedResponseToString(std::io::Error),
     #[error("Article parsing failed")]
     ArticleParseFailed(serde_json::Error),
+    #[error("Url parsing failed")]
+    UrlParseFailed(#[from] url::ParseError),
 }
 
 #[derive(Deserialize, Debug)]
@@ -31,4 +36,70 @@ pub fn get_articles(url: &str) -> Result<Articles, NewsApiError> {
     let articles: Articles =
         serde_json::from_str(&response).map_err(|e| NewsApiError::ArticleParseFailed(e))?;
     Ok(articles)
+}
+
+pub enum Endpoint {
+    TopHeadlines,
+}
+
+impl ToString for Endpoint {
+    fn to_string(&self) -> String {
+        match self {
+            Self::TopHeadlines => "top-headlines".to_string(),
+        }
+    }
+}
+
+pub enum Country {
+    Us,
+    Gr,
+    Jp,
+}
+
+impl ToString for Country {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Us => "us".to_string(),
+            Self::Gr => "gr".to_string(),
+            Self::Jp => "jp".to_string(),
+        }
+    }
+}
+
+struct NewsAPI {
+    api_key: String,
+    endpoint: Endpoint,
+    country: Country,
+}
+
+impl NewsAPI {
+    fn new(api_key: &str, country: Country) -> NewsAPI {
+        NewsAPI {
+            api_key: api_key.to_string(),
+            endpoint: Endpoint::TopHeadlines,
+            country: country,
+        }
+    }
+
+    fn endpoint(&mut self, endpoint: Endpoint) -> &mut NewsAPI {
+        self.endpoint = endpoint;
+        self
+    }
+
+    fn country(&mut self, country: Country) -> &mut NewsAPI {
+        self.country = country;
+        self
+    }
+
+    fn prepare_url(&self) -> Result<String, NewsApiError> {
+        let mut url = Url::parse(Base_URL)?;
+        url.path_segments_mut()
+            .unwrap()
+            .push(&self.endpoint.to_string());
+
+        let country = format!("country={}", self.country.to_string());
+        url.set_query(Some(&country));
+
+        Ok(url.to_string())
+    }
 }
