685a97fb3a7b3d982587b82386b8d0a6c3acc0a1
petrostrak <pit.trak@gmail.com>
2022-12-18 14:24:45 +0200

    Add header and footer widget

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,11 +1,14 @@
 use std::borrow::Cow;
 
 use eframe::{
-    egui::{CentralPanel, ScrollArea, Vec2},
+    egui::{
+        CentralPanel, CtxRef, Hyperlink, Label, ScrollArea, Separator, TextStyle, TopBottomPanel,
+        Ui, Vec2,
+    },
     epi::App,
     run_native, NativeOptions,
 };
-use headlines::Headlines;
+use headlines::{Headlines, PADDING};
 
 mod headlines;
 
@@ -21,15 +24,46 @@ impl App for Headlines {
 
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
         CentralPanel::default().show(ctx, |ui| {
+            render_header(ui);
             ScrollArea::auto_sized().show(ui, |ui| {
                 self.render_news_cards(ui);
-            })
+            });
+            render_footer(ctx);
         });
     }
 
     fn name(&self) -> &str {
         "Headlines"
     }
+}
+
+fn render_footer(ctx: &CtxRef) {
+    TopBottomPanel::bottom("footer").show(ctx, |ui| {
+        ui.vertical_centered(|ui| {
+            ui.add_space(10.);
+            ui.add(Label::new("API source: newsapi.org").monospace());
+            ui.add(
+                Hyperlink::new("https://github.com/emilk/egui")
+                    .text("Made with egui")
+                    .text_style(TextStyle::Monospace),
+            );
+            ui.add(
+                Hyperlink::new("https://github.com/petrostrak/headlines")
+                    .text("petrostrak/headlines")
+                    .text_style(TextStyle::Monospace),
+            );
+            ui.add_space(10.);
+        })
+    });
+}
+
+fn render_header(ui: &mut Ui) {
+    ui.vertical_centered(|ui| {
+        ui.heading("headlines");
+    });
+    ui.add_space(PADDING);
+    let sep = Separator::default().spacing(20.);
+    ui.add(sep);
 }
 
 fn main() {
