6ee8ef81886ebe4183cce0d1c0703bd4971e32b8
petrostrak <pit.trak@gmail.com>
2022-12-18 14:06:28 +0200

    Add custom font

--- headlines/src/main.rs
+++ headlines/src/main.rs
@@ -1,6 +1,10 @@
-use std::fmt::format;
+use std::{borrow::Cow, fmt::format};
 
-use eframe::{egui::CentralPanel, epi::App, run_native, NativeOptions};
+use eframe::{
+    egui::{CentralPanel, CtxRef, FontDefinitions, FontFamily, ScrollArea, Vec2},
+    epi::App,
+    run_native, NativeOptions,
+};
 
 struct Headlines {
     articles: Vec<NewsCardData>,
@@ -17,6 +21,28 @@ impl Headlines {
             articles: Vec::from_iter(iter),
         }
     }
+
+    fn configure_fonts(&self, ctx: &CtxRef) {
+        let mut font_def = FontDefinitions::default();
+        font_def.font_data.insert(
+            "MesloLGS".to_string(),
+            Cow::Borrowed(include_bytes!("../../MesloLGS_NF_Regular.ttf")),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Heading,
+            (FontFamily::Proportional, 35.),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Body,
+            (FontFamily::Proportional, 20.),
+        );
+        font_def
+            .fonts_for_family
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "MesloLGS".to_string());
+        ctx.set_fonts(font_def);
+    }
 }
 
 struct NewsCardData {
@@ -26,13 +52,24 @@ struct NewsCardData {
 }
 
 impl App for Headlines {
+    fn setup(
+        &mut self,
+        ctx: &eframe::egui::CtxRef,
+        _frame: &mut eframe::epi::Frame<'_>,
+        _storage: Option<&dyn eframe::epi::Storage>,
+    ) {
+        self.configure_fonts(ctx);
+    }
+
     fn update(&mut self, ctx: &eframe::egui::CtxRef, frame: &mut eframe::epi::Frame<'_>) {
         CentralPanel::default().show(ctx, |ui| {
-            for a in &self.articles {
-                ui.label(&a.title);
-                ui.label(&a.desc);
-                ui.label(&a.url);
-            }
+            ScrollArea::auto_sized().show(ui, |ui| {
+                for a in &self.articles {
+                    ui.label(&a.title);
+                    ui.label(&a.desc);
+                    ui.label(&a.url);
+                }
+            })
         });
     }
 
@@ -43,6 +80,7 @@ impl App for Headlines {
 
 fn main() {
     let app = Headlines::new();
-    let win_opts = NativeOptions::default();
+    let mut win_opts = NativeOptions::default();
+    win_opts.initial_window_size = Some(Vec2::new(540., 960.));
     run_native(Box::new(app), win_opts);
 }
